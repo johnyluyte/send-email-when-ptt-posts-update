6e244e2e5f7a13e4b2e3b8e07c4bb92f4e4582a8
Chien Chun <johnyluyte@gmail.com>
2015-09-21 15:28:10 +0800

    feature: can now parse author, title, board, date from the ptt article

--- index.js
+++ index.js
@@ -3,54 +3,86 @@ function alert_error(msg){
   alert(msg);
 }
 
-$("document").ready(function(){
-  $("#myForm").submit(function(){
+function isValidPttURL(url){
     // Check if the URL is REGEX correct (http(s)://www.ptt.cc/bbs/*).
-    var url = $("#ptt_url").val();
     ptt_regex = /http[s]?:\/\/www.ptt.cc\/bbs\/*/;
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
     if(url.match(ptt_regex)==null){
       alert_error("Error: Invalid URL! \n批踢踢文章網址錯誤。");
       return false;
     }
+}
 
+function isEmailNotEmpty(email){
     // We uses input type="email" so the browser will check the syntax of the e-mail for us.
     // However, we still need to check if the e-mail is empty.
-    if($("#email").val()==''){
+    if(email==''){
       alert_error("Error: Invalid E-mail! \n電子郵件欄位不可為空白");
       return false;
     }
+}
 
+function createJsonData(myForm){
     var data = {
-      "action": "get_article_title"
+      "action": "action_check_ptt"
     };
-    data = $(this).serialize() + "&" + $.param(data);
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      url: "response.php", //Relative or absolute path to response.php file
-      data: data,
-      success: function(data) {
-        if(data["error"]!=null){
-          alert_error(data["error"]);
-          return;
-        }
-        $(".the-return").html(
-          // data
-          "ptt_url: " + data["ptt_url"] +
-          "<br />ptt_title: " + data["ptt_title"] +
-          "<br />http_code: " + data["http_code"] +
-          "<br />errr: " + data["errr"] +
-          "<br />check_hour_period: " + data["check_hour_period"] +
-          "<br />check_total_days: " + data["check_total_days"] +
-          "<br />email: " + data["email"] +
-          "<br />JSON: " + data["json"]
-        );
+    /*
+      $(this).serialize() 會將 $(this)，也就是 <form>，裡面的 <input> 欄位與值變成 name: value 的 json 格式
+      在這個例子中，包含
+      "check_ptt_url":
+      "check_hour_period":
+      "check_total_days":
+      "email":
+    */
+    data = myForm.serialize() + "&" + $.param(data);
+    return data;
+}
 
-        // alert("Form submitted successfully.\nReturned json: " + data["json"]);
+function sendAjax(data){
+  $.ajax({
+    type: "POST",
+    dataType: "json",
+    url: "response.php", //Relative or absolute path to response.php file
+    data: data,
+    success: function(data) {
+      if(data["error"]!=null){
+        alert_error(data["error"]);
+        $("#the-return").html(data["error"]);
+        return;
       }
-    });
-    return false;
-  }); // .submit()
+      $("#the-return").html(
+        // data
+        "<br />article_title: " + data["article_title"] +
+        "<br />article_author: " + data["article_author"] +
+        "<br />article_board: " + data["article_board"] +
+        "<br />article_date: " + data["article_date"] +
+        "<br />check_ptt_url: " + data["check_ptt_url"] +
+        "<br />check_hour_period: " + data["check_hour_period"] +
+        "<br />check_total_days: " + data["check_total_days"] +
+        "<br />email: " + data["email"] +
+        "<br />article_last_push: " + data["article_last_push"]
+      );
+
+      // alert("Form submitted successfully.\nReturned json: " + data["json"]);
+    }
+  });
+}
+
+
+$("document").ready(function(){
+  $("#myForm").submit(function(event){
+
+    if(isValidPttURL($("#check_ptt_url").val())==false)
+      return;
+
+    if(isEmailNotEmpty($("#email").val())==false)
+      return;
+
+    sendAjax(createJsonData($(this)));
+
+    // https://api.jquery.com/submit/
+    // return false;  would does the same as event.preventDefault();
+    event.preventDefault();
+  });
 });
 
--- response.php
+++ response.php
@@ -1,15 +1,15 @@
 <?php
 /* Part of the codes are modified from https://github.com/jonsuh/jQuery-Ajax-Call-to-PHP-Script-with-JSON-Return */
 
+/* 安全性，檢查是否為 AJAX、是否為 POST 方法，並檢查 $action 是否正確*/
 if (is_ajax()) {
   if (isset($_POST["action"]) && !empty($_POST["action"])) { //Checks if action value exists
     $action = $_POST["action"];
     switch($action) { //Switch case for value of action
-      case "get_article_title": get_article_title(); break;
+      case "action_check_ptt": mainJob(); break;
     }
   }
 }
-
 
 //Function to check if the request is an AJAX request
 function is_ajax() {
@@ -17,28 +17,11 @@ function is_ajax() {
 }
 
 
-function get_article_title(){
-  $url = $_POST["ptt_url"];
-
-  $content = url_is_valid($url);
-  if($content=="false"){
-    // TODO echo alert when 404
-    echo json_encode(array('error' => 'An error occurred when parsing the url. (404 page not found)'));
-    return;
-  }
-
-  $return = $_POST;
-  $return["json"] = json_encode($return);
-  echo json_encode($return);
-
-}
-
-
 /*
-  Check if the URL is valid
+  Return the HTML content of the URL, return false if 404.
   e.g. https://www.ptt.cc/bbs/not_exist_board/not_exist_page.html is not valid
 */
-function url_is_valid($url){
+function get_url_content($url){
   $handle = curl_init($url);
   curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($handle, CURLINFO_HEADER_OUT, true);
@@ -67,6 +50,73 @@ function url_is_valid($url){
   /* Handle $response here. */
   return $response;
 }
+
+
+function get_next_article_metadata($content, $start_from){
+  /*
+    我們要的資訊(作者、看板、標題、時間)在的：
+      作者</span><span class="article-meta-value">uu26793 (Neural Damping)</span></div><div class="article-metaline-right"><span class="article-meta-tag">
+      看板</span><span class="article-meta-value">C_Chat</span></div><div class="article-metaline"><span class="article-meta-tag">
+      標題</span><span class="article-meta-value">Re: [閒聊] 台北市資訊局你....</span></div><div class="article-metaline"><span class="article-meta-tag">
+      時間</span><span class="article-meta-value">Wed Aug 12 10:43:59 2015</span>
+  */
+  $posValueStart = strpos($content, "article-meta-value", $start_from) + 20;  // add 20 to reach the "value"
+  $posValueEnd = strpos($content, "</span>", $posValueStart);
+  // http://www.w3schools.com/php/func_string_strpos.asp
+  // http://www.w3schools.com/php/func_string_substr.asp
+  return substr($content, $posValueStart, ($posValueEnd - $posValueStart) );
+}
+
+
+function mainJob(){
+  $url = $_POST["check_ptt_url"];
+
+  /*
+    如果沒有 404 的話，$content 的內容會是「整個 HTML」
+    e.g.
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width">
+        <meta name="robots" content="all" />
+        ...
+  */
+  $content = get_url_content($url);
+  if($content=="false"){
+    // TODO echo alert when 404
+    echo json_encode(array('error' => 'An error occurred when parsing the url. (404 page not found)'));
+    return;
+  }
+
+  // 抓出 PTT 文章的作者、看板、標題、時間
+  $posMetaDataStart = strpos($content, "main-content");
+  $content = substr($content, $posMetaDataStart);
+  $article_author = get_next_article_metadata($content, 0);
+  $article_board = get_next_article_metadata($content, strpos($content, $article_author));
+  $article_title = get_next_article_metadata($content, strpos($content, $article_board));
+  $article_date = get_next_article_metadata($content, strpos($content, $article_title));
+
+  // 抓出最後一篇推文
+  $postLastPushStart = strrpos($content, "push-tag") + 10;
+  $postLastPushEnd = strrpos($content, "</span></div></div>", $postLastPushStart);
+  $article_last_push = substr($content, $postLastPushStart, ($postLastPushEnd-$postLastPushStart) );
+
+  // 建立回傳的 JSON 物件
+  $return = $_POST;
+  $return["article_author"] = $article_author;
+  $return["article_board"] = $article_board;
+  $return["article_title"] = $article_title;
+  $return["article_date"] = $article_date;
+  $return["article_last_push"] = $article_last_push;
+
+  // 回傳 AJAX
+  echo json_encode($return);
+
+  // TODO: 存到 Database 裡面
+
+
+}
+
 
 
 ?>
